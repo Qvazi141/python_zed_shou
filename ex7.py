d7b094b48b65e562d3cd32e0c8891e82a96bb07a
Eugene Bulakhau <eugenebulakhau@localhost.localdomain>
2018-05-31 00:52:21 +0300

    ex7 done

---
+++ ex7.py
@@ -0,0 +1,19 @@
+# -*- encoding:utf-8 -*-
+
+print u"У Мэри был маленький барашек."
+print u"Его шерсть была белой как %s." % u'снег'
+print u"И всюду, куда Мэри шла,"
+print u"Маленький барашек всегда следовал за ней."
+print u"." * 10  # чтобы это могло значить?
+
+end1 = u"Б"
+end2 = u"а"
+end3 = u"д"
+end4 = u"д"
+end5 = u"и"
+end6 = u"г"
+end7 = u"а"
+end8 = u"й"
+
+print end1 + end2 + end3 + end4 + end5,
+print end6 + end7 + end8

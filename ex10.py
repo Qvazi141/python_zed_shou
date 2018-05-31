3f5d2dfaea7d651bd0dbb33a1c0f4f9987c952c8
Eugene Bulakhau <eugenebulakhau@localhost.localdomain>
2018-05-31 21:41:21 +0300

    Ex9, ex10 done

---
+++ ex10.py
@@ -0,0 +1,18 @@
+# -*- encoding:utf-8 -*-
+
+d_artagnan = u"\tМеня зовут д'Артаньян."
+athos = u"Это строка\nразделина на две"
+porthos = u"Я \\ - \\ мушкетер!"
+
+aramis = """
+Подсказки:
+\t* Граф из Гаскони
+\t* Связан с Миледи
+\t* Барон\n\t* Генерал Ордена
+"""
+
+print d_artagnan
+print athos
+print porthos
+print aramis
+
